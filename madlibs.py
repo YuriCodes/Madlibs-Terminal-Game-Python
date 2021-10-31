d3ed5cb3ff4d310fb1b654755723676c913b11c6
yuri <yuricodesbot@gmail.com>
2021-10-31 01:45:10 -0400

    first commit

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# String concatenation game
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2= input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me look forward to it because \
+I love to {verb1}. Remember to get enough rest, don't burn out and {verb2} like you are {famous_person}"
+
+print(madlib)
